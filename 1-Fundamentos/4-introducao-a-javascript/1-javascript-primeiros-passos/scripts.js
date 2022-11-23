008b005173cd09f8c4f7a40f0f3a1b68922685e2
Dev-Muller <pedromullergl11@gmail.com>
2022-11-23 19:10:46 -0300

    testes feitos

---
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,39 @@
+// Comecando exercicio 1
+let x = 6;
+let y = 2;
+ // adicao
+
+let z = x + y;
+console.log(z);
+
+// subtracao 
+
+let a = x - y;
+console.log(a);
+
+// multiplicacao
+
+let b = x * y;
+console.log(b);
+
+// divisao 
+
+let c = x / y;
+console.log(c);
+
+// modulo
+
+let d = x % y;
+console.log(d);
+
+let h = 10;
+
+let media = (x + y + h) / 3;
+
+console.log(media.toFixed(2));
+
+if (media >= 5) {
+    console.log('Aprovado');
+} else {
+    console.log('Reprovado');
+}
