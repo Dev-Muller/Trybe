7fd03506d023451bfbc0850f129037b8abb3f97e
Dev-Muller <pedromullergl11@gmail.com>
2022-11-25 15:43:30 -0300

    arrumado primeiro exercicio do para fixar

--- 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/Aula/script.js
+++ 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/Aula/script.js
@@ -30,7 +30,7 @@ console.log(tasksList);
 // Para Fixar!!
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu.indexOf('Serviços');
+let menuServices = menu.[1];
 
 console.log(menuServices);
 
