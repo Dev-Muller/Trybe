28f14c4d5643e18cf420804ca11633b4a3f69dcb
Dev-Muller <pedromullergl11@gmail.com>
2022-11-25 15:38:28 -0300

    Feito o Para Fixar da aula de array do modulo 4.2

--- 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/Aula/script.js
+++ 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/Aula/script.js
@@ -26,3 +26,18 @@ console.log(tasksList);
 
 tasksList.shift();
 console.log(tasksList);
+
+// Para Fixar!!
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+menu.push('Contatos');
+
+console.log(menu);
