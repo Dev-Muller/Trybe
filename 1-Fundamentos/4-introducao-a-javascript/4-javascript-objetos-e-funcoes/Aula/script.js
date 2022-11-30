9b80901474baa5e394a3efe793f089a8402aa343
Dev-Muller <pedromullergl11@gmail.com>
2022-11-30 16:01:17 -0300

    Feito o para fixar de objetos

--- 1-Fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/Aula/script.js
+++ 1-Fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/Aula/script.js
@@ -51,10 +51,59 @@ let usuario = {
         estado: 'Nova Iorque',
       },
     },
-  };
+};
   
-  console.log(usuario['id']); // 99
-  console.log(usuario.email); // jakeperalta@gmail.com
+console.log(usuario['id']); // 99
+console.log(usuario.email); // jakeperalta@gmail.com
   
-  console.log(usuario.infoPessoal.endereco.rua); // Smith Street
-  console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+
+  let moradores = [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+];
+  
+let primeiroMorador = moradores[0];
+console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+console.log(primeiroMorador['andar']); // 10
+  
+let ultimoMorador = moradores[moradores.length - 1];
+console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+console.log(ultimoMorador.nome); // Zoey
+
+// Para fixar Objetos
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+};
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]; // Ao tentar acessar uma chave inexistente o JavaScript automaticamente irá criar essa chave.
+
+console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
