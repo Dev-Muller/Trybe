e008b5086278b93ca58f9cb9e69eea980d6dc6c9
Dev-Muller <pedromullergl11@gmail.com>
2022-12-05 16:52:04 -0300

    exercicios de aula feitos

---
+++ 1-Fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicios/scripts.js
@@ -0,0 +1,66 @@
+/* O fatorial é representado pelo sinal !
+4! = 4 x 3 x 2 x 1 = 24 */
+
+for (let index = 10; index > 0; index -= 1) {
+  fatorial *= index;
+}
+console.log(fatorial);
+
+let word = 'tryber';
+
+// solucoes possiveis
+
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
+
+// segunda solucao
+
+let palavraContraria = '';
+
+palavraContraria = word.split('').reverse().join('');
+
+console.log(palavraContraria);
+
+// Segundo exercicio
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
+
+// terceiro exercicio
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
