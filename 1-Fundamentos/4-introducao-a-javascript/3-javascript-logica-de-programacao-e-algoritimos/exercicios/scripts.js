116c0857cfa1d3133c10f7bef2d2d57b2481de8c
Dev-Muller <pedromullergl11@gmail.com>
2022-12-05 16:56:13 -0300

    exercicios bonus concluidos

--- 1-Fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicios/scripts.js
+++ 1-Fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicios/scripts.js
@@ -64,3 +64,84 @@ for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
 }
 
 console.log(biggestPrimeNumber);
+
+// ex bonus
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
+
+let size = 5;
+let simbolo = '*';
+let linhaInput = '';
+
+for (let indexLine = 0; indexLine <= size; indexLine += 1) {
+  console.log(linhaInput);
+  linhaInput = linhaInput + simbolo;
+};
+ 
+let inputPosition = n - 1;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};
+
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
+
+let numero = 7;
+let middle = (n + 1) / 2;
+
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= numero; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
+
+let divisors = 1;
+let numberToCheck = 31;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo');
+else console.log(numberToCheck + ' não é primo');
