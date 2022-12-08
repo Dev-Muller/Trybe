c3baaaaf831e6a6c2b878cf39ccbb8d5578a95e7
Dev-Muller <pedromullergl11@gmail.com>
2022-12-08 16:00:02 -0300

    arrumando constantes do modulo de fundamentos 5.2

--- 1-Fundamentos/5-javascript-dom-eventos-webstorage/2-javascript-trabalhando-com-elementos/aula/script.js
+++ 1-Fundamentos/5-javascript-dom-eventos-webstorage/2-javascript-trabalhando-com-elementos/aula/script.js
@@ -34,7 +34,7 @@ irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
 pai.appendChild(irmaoElementoOndeVoceEsta); // Adicione o elemento criado como filho do elemento com o id `pai`
 
 // Crie um filho para `elementoOndeVoceEsta`.
-const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta'); // Recupere o elemento com o id elementoOndeVoceEsta
+const elementoOndeVcEsta = document.getElementById('elementoOndeVoceEsta'); // Recupere o elemento com o id elementoOndeVoceEsta
 const filhoElementoOndeVoceEsta = document.createElement('section'); // Crie um novo elemento 
 filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
 elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta); // Adicione o elemento criado como filho do elemento `elementoOndeVoceEsta`
@@ -46,7 +46,7 @@ filhoPrimeiroFilhoDoFilho.id = 'filhoPrimeiroFilhoDoFilho';
 primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho); // Adicione o elemento criado como filho do elemento `primeiroFilhoDoFilho`
 
 // A partir desse filho criado, acesse `terceiroFilho`.
-const terceiroFilho = filhoPrimeiroFilhoDoFilho
+const thirdFilho = filhoPrimeiroFilhoDoFilho
   .parentElement // primeiroFilhoDoFilho
   .parentElement // elementoOndeVoceEsta
   .nextElementSibling; // terceiroFilho
